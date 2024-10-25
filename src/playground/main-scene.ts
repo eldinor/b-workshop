15379d11ec53d1fb84879c11d1583632595f76d3
eldinor <eldinor@gmail.com>
2024-10-25 18:39:39 +0300

    radial cloner test

--- src/playground/main-scene.ts
+++ src/playground/main-scene.ts
@@ -41,6 +41,8 @@ import {
   TonemappingOperator,
   ConvolutionPostProcess,
   LensRenderingPipeline,
+  PostProcess,
+  Effect,
 } from "@babylonjs/core/";
 import "@babylonjs/loaders";
 // import { HtmlMesh, HtmlMeshRenderer } from "babylon-htmlmesh";
@@ -77,6 +79,7 @@ import { NiceLoader } from "./niceloader";
 import { wEffects } from "./effects";
 import { wSpeech } from "./speech";
 import { resolve } from "path";
+import { RadialCloner } from "@/clonerSystem/radialCloner";
 
 export default class MainScene {
   private camera: ArcRotateCamera;
@@ -916,6 +919,113 @@ export default class MainScene {
             document.getElementById("info")!.style.display = "none";
           }, 2000);
         }
+      }
+      //
+      if (keyName === "n" || keyName === "N") {
+        console.log("NNNN");
+        //    (this.scene.activeCamera! as ArcRotateCamera).upVector = Vector3.Down();
+
+        // PostProcess
+        Effect.ShadersStore["crtFragmentShader"] = `
+    #ifdef GL_ES
+    precision highp float;
+    #endif
+
+    #define PI 3.1415926538
+
+    varying vec2 vUV;
+    uniform sampler2D textureSampler;
+    uniform vec2 curvature;
+    uniform vec2 screenResolution;
+    uniform vec2 scanLineOpacity;
+    uniform float vignetteOpacity;
+    uniform float brightness;
+    uniform float vignetteRoundness;
+
+    vec2 curveRemapUV(vec2 uv) {
+    uv = uv * 2.0 - 1.0;
+    vec2 offset = abs(uv.yx) / vec2(curvature.x, curvature.y);
+    uv = uv + uv * offset * offset;
+    uv = uv * 0.5 + 0.5;
+    return uv;
+    }
+
+    vec4 scanLineIntensity(float uv, float resolution, float opacity) {
+    float intensity = sin(uv * resolution * PI * 2.0);
+    intensity = ((0.5 * intensity) + 0.5) * 0.9 + 0.1;
+    return vec4(vec3(pow(intensity, opacity)), 1.0);
+    }
+
+    vec4 vignetteIntensity(vec2 uv, vec2 resolution, float opacity, float roundness) {
+    float intensity = uv.x * uv.y * (1.0 - uv.x) * (1.0 - uv.y);
+    return vec4(vec3(clamp(pow((resolution.x / roundness) * intensity, opacity), 0.0, 1.0)), 1.0);
+    }
+
+    void main(void) {
+    vec2 remappedUV = vUV;
+    vec4 baseColor = texture2D(textureSampler, remappedUV);
+    baseColor *= vignetteIntensity(remappedUV, screenResolution, vignetteOpacity, vignetteRoundness);
+    baseColor *= scanLineIntensity(remappedUV.x, screenResolution.y, scanLineOpacity.x);
+    baseColor *= scanLineIntensity(remappedUV.y, screenResolution.x, scanLineOpacity.y);
+    baseColor *= vec4(vec3(brightness), 1.0);
+
+    if (remappedUV.x < 0.0 || remappedUV.y < 0.0 || remappedUV.x > 1.0 || remappedUV.y > 1.0) {
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    } else {
+        gl_FragColor = baseColor;
+    }
+    }
+    `;
+
+        var postProcess = new PostProcess(
+          "CRTShaderPostProcess",
+          "crt",
+          [
+            "curvature",
+            "screenResolution",
+            "scanLineOpacity",
+            "vignetteOpacity",
+            "brightness",
+            "vignetteRoundness",
+          ],
+          null,
+          1,
+          this.scene.activeCamera
+        );
+
+        postProcess.onApply = function (effect) {
+          effect.setFloat2("curvature", 5, 5);
+          effect.setFloat2("screenResolution", 300, 300);
+          effect.setFloat2("scanLineOpacity", 0.5, 0.5);
+          effect.setFloat("vignetteOpacity", 0.5);
+          effect.setFloat("brightness", 2.0);
+          effect.setFloat("vignetteRoundness", 10.0);
+        };
+      }
+      if (keyName === "u" || keyName === "U") {
+        console.log("UUUU");
+
+        const singleMeshes: any = [];
+
+        singleMeshesList.map((m) => {
+          return singleMeshes.push(this.scene.getMeshByName(m.name));
+        });
+        console.log(singleMeshes);
+
+        const cloned = (this.scene.getMeshByName("cooler") as Mesh).clone(
+          "cloned"
+        );
+        cloned.scaling.scaleInPlace(0.8);
+        cloned.bakeCurrentTransformIntoVertices();
+
+        const rc = new RadialCloner([cloned], this.scene, {
+          count: 20,
+          radius: 4,
+
+          isPickable: false,
+        });
+        cloned.setEnabled(true);
+        rc.root!.position = new Vector3(-5, 1, 0);
       }
       //
     }); // end event
